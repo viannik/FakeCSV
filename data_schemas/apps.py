d42ce619110fa5ce95d845cbbd75044e3878339b
viannik <55939563+viannik@users.noreply.github.com>
2023-03-09 12:51:36 +0200

    update migrations

--- data_schemas/apps.py
+++ data_schemas/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DataSchemasConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'data_schemas'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'data_schemas'
